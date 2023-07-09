2042dabae856bdd344429da91132a2659743341e
Alexey Kutin <aleks_413@mail.ru>
2023-07-09 22:51:15 +0300

    Add Avatar functionality (Model, Repository, Service, Controller)

---
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -0,0 +1,66 @@
+package ru.hogwarts.school.model;
+
+import javax.persistence.*;
+
+@Entity(name = "avatar")
+public class Avatar {
+    @Id
+    private Long id;
+    private String filePath;
+    private long fileSize;
+    private String mediaType;
+    private byte[] data;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private Student student;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+}
---
+++ src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
@@ -0,0 +1,59 @@
+package ru.hogwarts.school.service;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.exception.AvatarNotFoundException;
+import ru.hogwarts.school.model.Avatar;
+import ru.hogwarts.school.repository.AvatarRepository;
+import ru.hogwarts.school.repository.StudentRepository;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class AvatarServiceImpl implements AvatarService {
+    private final AvatarRepository avatarRepository;
+    private final StudentRepository studentRepository;
+
+    @Value("${application.avatars.folder}")
+    private String avatarFolder;
+
+    public AvatarServiceImpl(AvatarRepository avatarRepository, StudentRepository studentRepository) {
+        this.avatarRepository = avatarRepository;
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public Avatar findAvatarByStudentId(Long studentId) {
+        return avatarRepository.findById(studentId)
+                .orElseThrow(() -> new AvatarNotFoundException("The avatar not found for studentId = " + studentId));
+    }
+
+    @Override
+    public String uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
+        String avatarExtension = FilenameUtils.getExtension(avatarFile.getOriginalFilename());
+        Path avatarFilePath = Path.of(avatarFolder, studentId + "." + avatarExtension);
+        Files.createDirectories(avatarFilePath.getParent());
+        Files.deleteIfExists(avatarFilePath);
+
+        try (InputStream is = avatarFile.getInputStream()) {
+            Files.copy(is, avatarFilePath, StandardCopyOption.REPLACE_EXISTING);
+        }
+
+        Avatar avatar = new Avatar();
+        avatar.setId(studentId);
+        avatar.setFilePath(avatarFilePath.toString());
+        avatar.setFileSize(avatarFile.getSize());
+        avatar.setMediaType(avatarFile.getContentType());
+        avatar.setData(Files.readAllBytes(avatarFilePath));
+        avatar.setStudent(studentRepository.getReferenceById(studentId));
+
+        avatarRepository.save(avatar);
+        return avatarFilePath.toString();
+    }
+}
